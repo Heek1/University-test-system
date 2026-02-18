ad0326dc3d0645d48cca012f173de1d1a45a8abe
Speshni0k-Maksym <popovichspgmaster1@gmail.com>
2026-02-18 20:45:14 +0200

    Tests

---
+++ University test system/Views/Admin/CreateTest.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace University_test_system.Views.Admin
+{
+    public class CreateTestModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
