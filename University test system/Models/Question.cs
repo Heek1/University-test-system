2183870cd513c306f4d6098f7e4efec845b92a86
Ivalexz <daceeru@gmail.com>
2026-02-22 21:01:54 +0200

    Remove Test and Answers properties from Question model

--- University test system/Models/Question.cs
+++ University test system/Models/Question.cs
@@ -5,7 +5,5 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public int TestId { get; set; }
-        public Test Test { get; set; }
-        public List<Answer> Answers { get; set; }
     }
 }
