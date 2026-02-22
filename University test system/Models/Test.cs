e7b49b7e42d99614e4d91538e4fd2f526bdc70d0
Ivalexz <daceeru@gmail.com>
2026-02-22 21:03:12 +0200

    Refactor Test model by removing unused properties
    Removed properties 'AvailableFor', 'Subject', 'Questions', and 'Attempts' from Test model.

--- University test system/Models/Test.cs
+++ University test system/Models/Test.cs
@@ -5,11 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Level { get; set; }
-        public string AvailableFor { get; set; }
         public int SubjectId { get; set; }
-        public Subject Subject { get; set; }
         public int Time { get; set; }
-        public List<Question> Questions { get; set; }
-        public List<Attempt> Attempts { get; set; }
     }
 }
