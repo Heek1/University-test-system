090e862d4ad09eee7a0e7f9fff1dbd171205fd5e
Ivalexz <daceeru@gmail.com>
2026-02-22 20:59:57 +0200

    Remove User property from Attempt class

--- University test system/Models/Attempt.cs
+++ University test system/Models/Attempt.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string UserId { get; set; }
-        public User User { get; set; }
         public int TestId { get; set; }
         public Test Test { get; set; }
         public int Count { get; set; }      // кількість спроб
