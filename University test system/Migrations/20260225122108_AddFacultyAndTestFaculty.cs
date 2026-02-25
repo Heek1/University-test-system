ec8302e04e255cd512f7c3d7a2b8289e2e5c1bc3
Vlad <vladyuzevuts@gmail.com>
2026-02-25 14:40:24 +0200

    Fix bugs

---
+++ University test system/Migrations/20260225122108_AddFacultyAndTestFaculty.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University_test_system.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFacultyAndTestFaculty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
